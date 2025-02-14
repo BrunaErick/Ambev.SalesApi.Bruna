2a2dd264562b2b17de48de3c102408673b4064d8
TT_pessoal <TT_pessoal@LAPTOP-H63UOTE4>
2025-02-14 11:12:05 -0300

    Criados comandos no Migratior inicial para a criação das tabelas Carts e Products

--- Ambev.DeveloperEvaluation/src/Ambev.DeveloperEvaluation.ORM/Migrations/20241014011203_InitialMigrations.cs
+++ Ambev.DeveloperEvaluation/src/Ambev.DeveloperEvaluation.ORM/Migrations/20241014011203_InitialMigrations.cs
@@ -23,6 +23,41 @@ namespace Ambev.DeveloperEvaluation.ORM.Migrations
                     Status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
                     Role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
                 });
+
+            migrationBuilder.CreateTable(
+               name: "Products",
+               columns: table => new
+               {
+                   Id = table.Column<int>(nullable: false)
+                       .Annotation("SqlServer:Identity", "1, 1"),
+                   Title = table.Column<string>(nullable: false),
+                   Price = table.Column<decimal>(nullable: false),
+                   Description = table.Column<string>(nullable: true),
+                   Category = table.Column<string>(nullable: true),
+                   Image = table.Column<string>(nullable: true),
+                   RatingRate = table.Column<decimal>(nullable: false),
+                   RatingCount = table.Column<int>(nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Products", x => x.Id);
+               });
+
+            migrationBuilder.CreateTable(
+               name: "Carts",
+               columns: table => new
+               {
+                   Id = table.Column<int>(nullable: false)
+                       .Annotation("SqlServer:Identity", "1, 1"),
+                   UserId = table.Column<int>(nullable: false),
+                   Date = table.Column<DateTime>(nullable: false)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_Carts", x => x.Id);
+               });
+
+
         }
 
         /// <inheritdoc />
@@ -30,6 +65,12 @@ namespace Ambev.DeveloperEvaluation.ORM.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Users");
+
+            migrationBuilder.DropTable(
+            name: "Products");
+
+            migrationBuilder.DropTable(
+            name: "Carts");
         }
     }
 }
--- Ambev.DeveloperEvaluation/src/Ambev.DeveloperEvaluation.WebApi/Migrations/20250211211246_Inicial.cs
+++ Ambev.DeveloperEvaluation/src/Ambev.DeveloperEvaluation.WebApi/Migrations/20250211211246_Inicial.cs
@@ -25,6 +25,42 @@ namespace Ambev.DeveloperEvaluation.WebApi.Migrations
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 });
+
+
+            migrationBuilder.CreateTable(
+              name: "Products",
+              columns: table => new
+              {
+                  Id = table.Column<int>(nullable: false)
+                      .Annotation("SqlServer:Identity", "1, 1"),
+                  Title = table.Column<string>(nullable: false),
+                  Price = table.Column<decimal>(nullable: false),
+                  Description = table.Column<string>(nullable: true),
+                  Category = table.Column<string>(nullable: true),
+                  Image = table.Column<string>(nullable: true),
+                  RatingRate = table.Column<decimal>(nullable: false),
+                  RatingCount = table.Column<int>(nullable: false)
+              },
+              constraints: table =>
+              {
+                  table.PrimaryKey("PK_Products", x => x.Id);
+              });
+
+            migrationBuilder.CreateTable(
+              name: "Carts",
+              columns: table => new
+              {
+                  Id = table.Column<int>(nullable: false)
+                      .Annotation("SqlServer:Identity", "1, 1"),
+                  UserId = table.Column<int>(nullable: false),
+                  Date = table.Column<DateTime>(nullable: false)
+              },
+              constraints: table =>
+              {
+                  table.PrimaryKey("PK_Carts", x => x.Id);
+              });
+
+
         }
 
         /// <inheritdoc />
@@ -32,6 +68,12 @@ namespace Ambev.DeveloperEvaluation.WebApi.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Users");
+
+            migrationBuilder.DropTable(
+            name: "Products");
+
+            migrationBuilder.DropTable(
+            name: "Carts");
         }
     }
 }
